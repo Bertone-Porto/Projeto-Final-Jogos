96cf38c52aa4cb1419da089570af2c5e2bd664f2
Bertone-Porto <38756105+Bertone-Porto@users.noreply.github.com>
2023-01-17 15:48:17 -0300

    Update and rename manu.c to menu.c

--- menu.c
+++ menu.c
@@ -14,32 +14,35 @@ int main (int argc, char* args[])
 
     /* INICIALIZACAO */
     SDL_Init(SDL_INIT_EVERYTHING);
-    SDL_Window* win = SDL_CreateWindow("Contando o Tempo",
+    SDL_Window* win = SDL_CreateWindow("Menu",
                          SDL_WINDOWPOS_UNDEFINED,
                          SDL_WINDOWPOS_UNDEFINED,
-                         640, 480, SDL_WINDOW_SHOWN
+                         500, 500, SDL_WINDOW_SHOWN
                       );
     SDL_Renderer* ren = SDL_CreateRenderer(win, -1, 0);
     TTF_Init();
-    SDL_Color padrao = { 255,255,255,255 };
+    SDL_Color padrao = { 255,231,55,255 };
     SDL_Color focus = { 237,139,0,255 };
 
     TTF_Font *ourFont = TTF_OpenFont("Pacmania.ttf",100);
     
-    struct SDL_Surface* listaSurfaceText[2];
+    struct SDL_Surface* listaSurfaceText[3];
     listaSurfaceText[0] = TTF_RenderText_Solid(ourFont, "Play",padrao);  
     listaSurfaceText[1] = TTF_RenderText_Solid(ourFont, "Quit",padrao); 
-     
-    struct SDL_Texture* listaTextureText[2];
+    listaSurfaceText[2] = TTF_RenderText_Solid(ourFont, "PACMAN",padrao); 
+ 
+    struct SDL_Texture* listaTextureText[3];
 	listaTextureText[0] = SDL_CreateTextureFromSurface(ren,listaSurfaceText[0]);
     listaTextureText[1] = SDL_CreateTextureFromSurface(ren,listaSurfaceText[1]);
+	listaTextureText[2] = SDL_CreateTextureFromSurface(ren,listaSurfaceText[2]);
 
 	SDL_Surface* bertoneSur = TTF_RenderText_Solid(ourFont, "bertone",padrao);
     SDL_Texture* bertoneTex = SDL_CreateTextureFromSurface(ren,bertoneSur);
     int i;
     SDL_Point mouse = {0,0};
-    SDL_Rect recPlay = {270,120,100,30};
-    SDL_Rect recQuit = {270,200,100,30};
+    SDL_Rect recPlay = {200,120,100,30};
+    SDL_Rect recQuit = {200,200,100,30};
+	SDL_Rect recTitle = {110, 50, 300,50};
     bool selecionado = false;
     bool gameIsRunning = true;
      /* EXECUÇÃO */
@@ -90,10 +93,11 @@ int main (int argc, char* args[])
 		
 		SDL_RenderCopy(ren,listaTextureText[0],NULL,&recPlay);
 		SDL_RenderCopy(ren,listaTextureText[1],NULL,&recQuit);
+		SDL_RenderCopy(ren,listaTextureText[2],NULL,&recTitle);
 		SDL_RenderPresent(ren);
 	}	
 
-	for(i = 0; i < 2;i++){
+	for(i = 0; i < 3;i++){
 		SDL_FreeSurface(listaSurfaceText[i]);
 		SDL_DestroyTexture(listaTextureText[i]);
 	}
